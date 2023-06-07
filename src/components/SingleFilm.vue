ca8b2b8b08ee82b2bcf8c44102212d1c5a66d6fb
gabrielemafrica <gabriele.ga@live.it>
2023-06-07 19:04:58 +0300

    flags

--- src/components/SingleFilm.vue
+++ src/components/SingleFilm.vue
@@ -1,9 +1,27 @@
 <script>
+import { store } from '../store';
 
 export default {
   name: 'SingleFilm',
   props: {
     filmObj: Object 
+  },
+  data() {
+    return {
+      store,
+    }
+  },
+  methods: {
+    flagURLmounter(inizioUrl, linguaFilm, fineURL) {
+      let basicURL = inizioUrl;
+      let lingua = linguaFilm;
+      // lingua === 'en' ? lingua = 'us' : lingua = lingua;
+      if (lingua === 'en') {
+        lingua = 'us';
+      }
+      basicURL +=  `${lingua}${fineURL}`;
+      return basicURL;
+    }
   }
 }
 
@@ -22,6 +40,10 @@ export default {
   <h4>
     {{ filmObj.original_language }}
   </h4>
+  <div class="flag">
+   <img class="img-fluid mb-3" :src="flagURLmounter(store.flagURL, filmObj.original_language, store.flagIMG)" onerror="this.onerror=null;this.src='https://png.pngtree.com/thumb_back/fh260/background/20220509/pngtree-404-error-page-not-found-image_1345267.jpg'; " :alt="filmObj.original_language">
+  </div>
+  
   <span>Voto</span>
   <h4>
     {{ filmObj.vote_average }}
@@ -34,4 +56,7 @@ export default {
 @use '../styles/partials/variables' as *;
 @use '../styles/general.scss' ;
 
+.flag{
+  width: 100px;
+}
 </style>
